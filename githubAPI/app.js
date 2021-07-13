514f7136326469de6972b0139365acaed98a5f1c
Franziska <franziska@Franziskas-MacBook-Pro.fritz.box>
2021-07-13 14:25:23 +0200

    handling the requests

--- githubAPI/app.js
+++ githubAPI/app.js
@@ -1,1 +1,15 @@
-console.log("Hello, World!");
+import axios from "axios";
+
+//handling the form
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("form");
+  form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const username = document.querySelector("input").value;
+    //handling the requests
+    const response = await axios.get(
+      `https://api.github.com/users/${username}`
+    );
+    console.log(response.data);
+  });
+});
