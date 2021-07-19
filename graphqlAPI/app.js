da6894c9657abe4fb336284452eab9709f230e95
Franziska <franziska@Franziskas-MBP.fritz.box>
2021-07-19 14:40:12 +0200

    adding graphql

--- graphqlAPI/app.js
+++ graphqlAPI/app.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const graphqlHTTP = require("express-graphql").graphqlHTTP;
 const app = express();
+
+app.use("/graphql", graphqlHTTP());
 
 app.listen(3000, () => {
   console.log("App listening on port 3000");
